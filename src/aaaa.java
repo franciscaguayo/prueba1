765c52228fa647ca38acca3bb5d69408f1c6a239
francisca <f.aguayo03@ufromail.cl>
2025-04-11 12:37:16 -0400

    kk nn

--- src/aaaa.java
+++ src/aaaa.java
@@ -2,6 +2,6 @@ public class aaaa {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
 
-        System.out.println("Francisca Aguayo");
+        System.out.println("Francis ca");
     }
 }
